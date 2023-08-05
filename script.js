ccd733e4b2c3373178cbfe37498795f25cefb20a
Pravallikaa <pravallikaamohan@gmail.com>
2023-08-05 18:51:54 +0530

    Add Project files

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container")
 function addTask() {
     if (inputBox.value === '') {
-        alert("You must write something!");
+        alert("Enter a task!");
     }
     else {
         let li = document.createElement("li");
